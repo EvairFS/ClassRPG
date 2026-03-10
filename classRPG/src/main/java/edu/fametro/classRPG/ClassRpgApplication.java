4960356ca383b670af87053f198ee9ce1e7521f1
Evair Ferreira Siqueira Filho <EvairFS>
2026-03-09 20:39:27 -0400

    initial commit

---
+++ classRPG/src/main/java/edu/fametro/classRPG/ClassRpgApplication.java
@@ -0,0 +1,13 @@
+package edu.fametro.classRPG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassRpgApplication.class, args);
+	}
+
+}
---
+++ classRPG/src/test/java/edu/fametro/classRPG/ClassRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fametro.classRPG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
