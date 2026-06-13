805d4432d82acc1ad593c86a9ffaacb0c7a7faff
Evair Ferreira Siqueira Filho <evair.juninho@gmail.com>
2026-06-13 16:40:26 -0400

    agora foi vai

--- API/prisma.config.ts
+++ API/prisma.config.ts
@@ -1,14 +1,13 @@
-import "dotenv/config"
-import { defineConfig } from "prisma/config"
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config"; // 💡 Importado o 'env' nativo do Prisma
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    // @ts-ignore
-    url: process.env.DATABASE_URL,
+    url: env("DATABASE_URL"), // ✨ Sem a necessidade de @ts-ignore
   },
   migrations: {
-    // ⚔️ Adicionado o 'npx' na frente para o Windows encontrar o executor local
+    // ⚔️ npx garante a execução correta tanto local (Windows) quanto no Render (Linux)
     seed: "npx ts-node ./prisma/seed.ts", 
   },
-})
+});
